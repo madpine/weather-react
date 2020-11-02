9fdd28ead4b1b495d3ced66cdbd9cc251c5f8e5f
madpine <maddiepine96@gmail.com>
2020-11-02 10:39:11 +0200

    Changed container background color

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h1>
-          Hello World
-        </h1>
-        <Weather city="Paris"/>
+        <Weather />
       </header>
     </div>
   );
