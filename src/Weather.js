a1a046c3bbbd3ef4bd278995379a6b2b512a5794
madpine <maddiepine96@gmail.com>
2020-11-02 10:35:41 +0200

    Imported Weather App JS and CSS from CodeSandbox

--- src/Weather.js
+++ src/Weather.js
@@ -1,21 +1,87 @@
 import React from 'react';
 import axios from 'axios';
 import Loader from 'react-loader-spinner';
+import './Weather.css';
 
-export default function Weather(props) {
 
-    function handleResponse(response) {
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp}`)
-    }
-    let apiKey = "669df815a230d0606e20716b21beda24";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse)
-    return ( <Loader
-         type="Puff"
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000} //3 secs
- 
-      />);
+export default function Weather() {
+  let weatherData = {
+    city: "Tel Aviv",
+    date: "October 12, 2020",
+    time: "12:30",
+    temperature: 82,
+    description: "Cloudy",
+    humidity: 56,
+    wind: 5
+  };
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="card-body whole-card shadow-sm">
+          <form>
+            <div className="row">
+              <div className="col-7">
+                <input
+                  type="search"
+                  name="City"
+                  className="city-search"
+                  placeholder="Search for a city..."
+                  autocomplete="off"
+                />
+              </div>
+              <div className="col-2">
+                <input
+                  type="submit"
+                  value="Search"
+                  className="search-button shadow-sm"
+                />
+              </div>
+              <div className="col-2"> </div>
+            </div>
+          </form>
+          <button className="search-button current-button shadow-sm">
+            Current
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-7">
+          <h1 className="current-city">{weatherData.city}</h1>
+          <p className="date-time">
+            {weatherData.date} | {weatherData.time}
+          </p>
+          <div className="row">
+            <p className="details">
+              <ul>
+                <li>Humidity: {weatherData.humidity}%</li>
+                <li>Wind: {weatherData.wind}mph</li>
+                <li>Description: {weatherData.description}</li>
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="col-5">
+          <div className="row">
+            <h2>
+              {weatherData.temperature}
+              <span className="unit">
+                <a href="#"> ˚C </a> | <a href="#"> ˚F </a>
+              </span>
+            </h2>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card-body weekly-card">
+          <div className="row"></div>
+        </div>
+      </div>
+      <p className="github-link">
+        <a href="https://github.com/madpine/weather-react" target="_blank">
+          Open-source code
+        </a>{" "}
+        by Maddie Pine
+      </p>
+    </div>
+  );
 }
