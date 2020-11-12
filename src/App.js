4b9975b201413f6014521a657b145308816e1619
madpine <maddiepine96@gmail.com>
2020-11-12 12:10:13 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
         <Weather />
         <footer>
           This project is {" "}
           <a href="https://github.com/madpine/weather-react" target="_blank">open-sourced</a>
           {" "} and was coded by Maddie Pine
         </footer>
+        </div>
     </div>
   );
 }
